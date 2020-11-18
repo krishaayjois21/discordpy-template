781e3b6bfcbbd55ee436cdc78763d0a8dee4b15d
Krishaay Jois <62088268+krishaayjois21@users.noreply.github.com>
2020-11-18 23:24:38 +0530

    Initial Commit

---
+++ extensions/ready.py
@@ -0,0 +1,33 @@
+# Import required modules
+
+from discord import activity
+from discord.ext import commands
+from discord import ActivityType, Activity, Status
+from json import load
+
+# Load Configuration
+with open("config/config.json", "r") as conf:
+    config = load(conf)
+    prefix = config["prefix"]
+
+# Change as you need. This is the status for the bot
+type = ActivityType.playing
+name = f"Prefix: {prefix}"
+status = Status.online
+act = Activity(name=name, type=type)
+
+# Create the class for the extension
+class InitListener(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        # Change the bot status to what we mention above
+        await self.client.change_presence(activity=activity, status=status)
+        print("Bot is online")
+
+
+# Function for discord.py to load this extension
+def setup(client):
+    client.add_cog(InitListener(client))
---
+++ main.py
@@ -0,0 +1,59 @@
+"""
+discord.py template
+Date: 18/11/2020
+Author: Krishaay Jois
+Github: https://github.com/krishaayjois21/discordpy-template
+
+Note: Before running the bot enter configuration details in config/config.json
+
+LICENSE:
+MIT License
+
+Copyright (c) 2020 Krishaay Jois
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+# Import required modules
+from json import load
+
+from discord import client
+from discord.ext import commands
+
+
+# Load configuration data from config/config.json
+
+with open("config/config.json", "r") as conf:
+    config = load(conf)
+    prefix = config["prefix"]
+    token = config["token"]
+    extensions = config["extensions"]
+
+
+# Create a bot instance with the prefix from the configuration
+client = commands.Bot(command_prefix=prefix)
+
+
+# Load all the extensions mentioned in
+for extension in extensions:
+    client.load_extension(extension)
+
+
+# Run the bot
+client.run(token)
