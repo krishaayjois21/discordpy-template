93a3038660323d69b76d4f5b8434477d151262a7
Krishaay Jois <krishaayjois@gmail.com>
2020-11-29 22:33:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,3 @@
-"""
-discord.py template
-Date: 18/11/2020
-Author: Krishaay Jois
-Github: https://github.com/krishaayjois21/discordpy-template
-
-Note: Before running the bot enter configuration details in config/config.json
-
-LICENSE:
-MIT License
-
-Copyright (c) 2020 Krishaay Jois
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -54,7 +31,7 @@ client = commands.Bot(command_prefix=prefix)
 for extension in extensions:
     try:
         client.load_extension(extension)
-        print(f"『LOADED』『✓』 {extension.split('.')[-1]} LOADED FROM {extension}")
+        print(f"『EXTENSION』『✓』 EXTENSION '{extension.split('.')[-1]}' LOADED FROM '{extension}'")
     except Exception:
         print(
             f"『FAILED』『×』 {extension.split('.')[-1]} COULD NOT BE LOADED FROM {extension}"
